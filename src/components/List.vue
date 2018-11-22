bb34f789d781de2fc9d2a96441d2f96b93e0ff7d
@FullZero <fullzero5@gmail.com>
2018-11-23 00:18:25 +0300

    add scss style
    add scss style
    chenge fonts
    add scope

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
     <h1>Товары</h1>
-    <ul>
-      <li v-for="item in Products" v-bind:key="item.id">
-        {{ item.text }} - {{ item.cost }} ₽<button @click="addToItems(item);">
-          В корзину
-        </button>
+    <ul class="ProductSet" v-bind:class="view">
+      <li
+        v-for="item in Products"
+        v-bind:key="item.id"
+        class="ProductCard"
+        v-bind:class="view_c"
+      >
+        <div class="ProductCard__img-wrapper">
+          <img
+            :src="item.img + '2000'"
+            :alt="item.text"
+            class="ProductCard__img"
+          />
+        </div>
+        <div class="ProductCard__details">
+          <div class="ProductCard__details__header">
+            <div class="ProductCard__titles">
+              <h4 class="ProductCard__title">{{ item.text }}</h4>
+              <h5 class="ProductCard__price">{{ item.cost }} ₽</h5>
+            </div>
+            <button @click="addToItems(item);" class="IconBtn">
+              В корзину
+            </button>
+          </div>
+          <p class="ProductCard__description">
+            Описание товара {{ item.text }} - текст текст текст
+          </p>
+        </div>
       </li>
     </ul>
   </div>
@@ -15,6 +38,12 @@
 import { mapActions, mapGetters } from "vuex";
 export default {
   name: "List",
+  data() {
+    return {
+      view: "ProductSet--grid",
+      view_c: "ProductCard--grid"
+    };
+  },
   computed: {
     ...mapGetters(["Products"])
   },
--- src/store/index.js
+++ src/store/index.js
@@ -9,19 +9,49 @@ export default {
         id: 1,
         text: "lorem1",
         cost: 100,
-        img: "https://via.placeholder.com/90"
+        img: "https://via.placeholder.com/"
       },
       {
         id: 2,
         text: "lorem2",
         cost: 200,
-        img: "https://via.placeholder.com/90"
+        img: "https://via.placeholder.com/"
       },
       {
         id: 3,
         text: "lorem3",
         cost: 300,
-        img: "https://via.placeholder.com/90"
+        img: "https://via.placeholder.com/"
+      },
+      {
+        id: 4,
+        text: "lorem4",
+        cost: 400,
+        img: "https://via.placeholder.com/"
+      },
+      {
+        id: 5,
+        text: "lorem5",
+        cost: 400,
+        img: "https://via.placeholder.com/"
+      },
+      {
+        id: 6,
+        text: "lorem6",
+        cost: 1400,
+        img: "https://via.placeholder.com/"
+      },
+      {
+        id: 7,
+        text: "lorem7",
+        cost: 900,
+        img: "https://via.placeholder.com/"
+      },
+      {
+        id: 8,
+        text: "lorem8",
+        cost: 1900,
+        img: "https://via.placeholder.com/"
       }
     ],
     cart: {},
